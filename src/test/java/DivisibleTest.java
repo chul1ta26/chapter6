787217386e850a6f41ab954d63fad8a80a21b02d
chul1ta26 <dayhany26@gmail.com>
2019-10-16 23:57:31 -0400

    isDivisible test

--- src/test/java/DivisibleTest.java
+++ src/test/java/DivisibleTest.java
@@ -1,2 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class DivisibleTest {
+    Divisible divisible = new Divisible();
+
+    @Test
+    public void isDivisibleTest(){
+        Boolean result = divisible.isDivisible(9,3);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void isDivisibleTest1(){
+        Boolean result = divisible.isDivisible(9,2);
+        Assert.assertFalse(result);
+    }
 }
