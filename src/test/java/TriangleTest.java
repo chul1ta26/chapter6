17de46ae66e3349e4e490ff4203c7eb41e2eda37
chul1ta26 <dayhany26@gmail.com>
2019-10-17 00:16:30 -0400

    isTriangle test assert false

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -6,8 +6,15 @@ public class TriangleTest {
 
     @Test
     public void isTriangleTest(){
-      boolean result = triangle.isTriangle(7,10,5);
+        boolean result = triangle.isTriangle(7,10,5);
 
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void isTriangleTest1(){
+        boolean result = triangle.isTriangle(1,10,5);
+
+        Assert.assertFalse(result);
     }
 }
